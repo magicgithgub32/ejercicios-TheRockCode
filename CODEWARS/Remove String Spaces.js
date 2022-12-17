df46b257f622629ec4408e3ad12fe2b5ef668549
Rubcs <rubcastellon@gmail.com>
2022-12-17 06:41:48 +0100

    create CODEWARS directory and add ex-1

---
+++ CODEWARS/Remove String Spaces.js
@@ -0,0 +1,12 @@
+
+//INSTRUCTIONS
+// Simple, remove the spaces from the string, then return the resultant string.
+
+
+// function noSpace(x){
+// }
+
+
+function noSpace(x){
+    return x.replace(/\s/g, '');
+  }
