7de324289bdccfe35205f6679d0bd92c3f62f089
Rubcs <rubcastellon@gmail.com>
2022-12-22 13:07:12 +0100

    create ex-01 in REACT

---
+++ REACT/01-jsx-interpolacion/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+const school = 'Rock{theCode}';
+const chapters = 6;
+const isAwesome = true;
+const technologies = ['JavaScript', 'React', 'Node'];
+
+const profile = {
+  job: 'Web developer',
+  description:
+    '¡Me encanta la programación y el desarrollo web!',
+};
+
+
+function App() {
+
+  return (
+    <div className="App">
+
+      
+      <h1>Estudio en {school}</h1>
+      <p>Veremos un total de {chapters} módulos de contenido</p>
+      <p>Lo que estoy aprendiendo en {school} está siendo {isAwesome === true ? 'increíble' : 'top'}</p>
+      
+      {technologies.map((technology, index) => (
+      <ul> Aprendemos a usar {technology} </ul>
+    ))}
+
+      <h3>Quiero ser {profile.job} y {profile.description}</h3>    
+    </div>
+  )
+}
+
+export default App
