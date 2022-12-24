e2f2e743c6aefa3e2024bcb9eeac007407dd446c
Rubcs <rubcastellon@gmail.com>
2022-12-24 11:57:31 +0100

    add ex-03 to REACT

---
+++ REACT/03-listas-props/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Food from "./components/Food";
+
+function App() {
+  const foods = [
+    { name: "pizza", calories: 1400 },
+    { name: "hamburger", calories: 1000 },
+    { name: "fish and chips", calories: 600 },
+    { name: "salad", calories: 400 },
+  ];
+
+  return (
+    <div className="App">
+      <h3>Menu</h3>
+      <ul>
+        {foods.map((food) => {
+          return <Food calories={food.calories} name={food.name} />;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
