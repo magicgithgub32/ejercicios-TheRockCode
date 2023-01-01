b3534032329d8591111d99c36c273d0d9e49a1b1
Rubcs <rubcastellon@gmail.com>
2023-01-01 12:10:40 +0100

    add content to ex-10 REACT

--- REACT/10-Efectos-III/src/App.jsx
+++ REACT/10-Efectos-III/src/App.jsx
@@ -7,19 +7,56 @@ function App() {
 
   const [backgroundColor, setBackgroundColor] = useState('white')
 
+  const [color, setColor] = useState('red')
+
+  const [fontSize, setFontSize] = useState('16')
+
+  const [borderRadius, setBorderRadius] = useState(4);
+
+
   useEffect(() => {
-    let newStyles = {backgroundColor} // {backgroundColor : backgroundColor}
-    setStyle(newStyles)
-  }, [backgroundColor])
+    // let newStyles = {backgroundColor} 
+    setStyle({backgroundColor: backgroundColor, // setStyle({newStyles})
+     color: color,
+     fontSize: `${fontSize}px`,
+     borderRadius: `${borderRadius}px`
+    })
+  }, [backgroundColor, color, fontSize, borderRadius])
+
 
   return (
     <div className="App">
       <h2>Editando estilos de forma dinámica.</h2>
-      <button  style={style} >Estilos dinámicos</button>
+      <button  style={style}>Cambia el color de fondo del botón</button>
       <input 
       type="color"
       label="Selecciona el color de fondo"
       onChange={(e) => setBackgroundColor(e.target.value)}  />
+
+      <br />
+
+      <button>Cambia el color del texto</button>
+      <input 
+      type="color"
+      label="selecciona el color del texto del botón"
+      onChange={(e) => setColor(e.target.value)}  />
+
+      <br />
+
+      <button>Cambia el tamaño del texto</button>
+      <input 
+      type="number"
+      label="selecciona el tamaño del texto" 
+      onChange={(e) => setFontSize(e.target.value)}/>
+
+      <br />
+
+      <button>Cambia el estilo del borde del botón</button>
+      <input 
+      type="number"
+      label="selecciona el estilo del borde del botón"
+      onChange={(e) => setBorderRadius(e.target.value)}/>
+
       </div>
   )
 }
