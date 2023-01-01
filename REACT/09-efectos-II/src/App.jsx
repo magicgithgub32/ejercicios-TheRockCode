d05d40f08a98e2fba860674b849e4cd878e980a5
Rubcs <rubcastellon@gmail.com>
2023-01-01 16:45:17 +0100

    modify content to ex-09 REACT

--- REACT/09-efectos-II/src/App.jsx
+++ REACT/09-efectos-II/src/App.jsx
@@ -1,63 +1,67 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
-import './App.css'
-
-function App() {
-
-  const [time, setTime] = useState('');
-
-  //Creo el objeto Date
-  let today = new Date();
-// Obtengo la hora
-  let timeNow = today.toLocaleTimeString();
-
-//Creo intervalo para cada segundo, donde actualizo Time con la hora
-  const intervalRefresh = setInterval(() => {
-    setTime(timeNow)
-  }, 1000);
-
-  //monto el componente con la hora actual y elimino el intervalo tras montarlo. Escucho a la variable del intervalo, 
-  //para que cuando cambie al segundo, vuelva a montar el componente y a desmontarlo
-  useEffect(()=>{
-    setTime(timeNow)
-  return () => {
-    clearInterval(intervalRefresh)
-  }
-  },[intervalRefresh])
-
-//Lo renderizo
-  return (
-    <div className="App">
-      <h1>Live TIME:  {time}</h1>
-    </div>
-  )
-}
-
-export default App
-
-
-// import { useState, useEffect } from 'react';
-// import './App.css';
+// import { useEffect } from 'react';
+// import { useState } from 'react';
+// import './App.css'
 
 // function App() {
+
 //   const [time, setTime] = useState('');
 
-//   useEffect(() => {
-//     const interval = setInterval(
-//       () => setTime(new Date().toLocaleTimeString()),
-//       1000
-//     );
+//   //Creo el objeto Date
+//   let today = new Date();
+// // Obtengo la hora
+//   let timeNow = today.toLocaleTimeString();
 
-//     return () => {
-//       clearInterval(interval);
-//     };
-//   }, []);
+// //Creo intervalo para cada segundo, donde actualizo Time con la hora
+//   const intervalRefresh = setInterval(() => {
+//     setTime(timeNow)
+//   }, 1000);
 
+//   //monto el componente con la hora actual y elimino el intervalo tras montarlo. Escucho a la variable del intervalo, 
+//   //para que cuando cambie al segundo, vuelva a montar el componente y a desmontarlo
+//   useEffect(()=>{
+//     setTime(timeNow)
+//   return () => {
+//     clearInterval(intervalRefresh)
+//   }
+//   },[intervalRefresh])
+
+// //Lo renderizo
 //   return (
 //     <div className="App">
-//       <h1>{time}</h1>
+//       <h1>Live TIME:  {time}</h1>
 //     </div>
-//   );
+//   )
 // }
 
-// export default App;
+// export default App
+
+
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [time, setTime] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => setTime(new Date().toLocaleTimeString()),
+      1000
+    );
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <h1 >{time}</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
