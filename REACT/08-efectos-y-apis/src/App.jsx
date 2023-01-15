7117e6ea3b5aa4568b0bcfa7d21c681b00d8fc3b
Rubcs <rubcastellon@gmail.com>
2023-01-15 11:02:55 +0100

    add content to ex-08 in REACT

--- REACT/08-efectos-y-apis/src/App.jsx
+++ REACT/08-efectos-y-apis/src/App.jsx
@@ -1,34 +1,30 @@
-import './App.css'
-import { useEffect, useState } from 'react'
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-
-  const [characters, setCharacters] = useState([])
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     fetch(`https:rickandmortyapi.com/api/character`)
-    .then((res) => res.json())
-    .then((apiData) => {
-      setCharacters (apiData.results)
-    })
-  }, [])
+      .then((res) => res.json())
+      .then((apiData) => {
+        setCharacters(apiData.results);
+      });
+  }, []);
 
-console.log(characters)
-  
-return (
-  <div className="App">
-    {characters.map((character) => (
-      <div key={character.id}>
-        <h2>{character.name}</h2>
-        <p>{character.species}</p>
-        <img src={character.image} alt={character.name} />
-      </div>
-    ))}
-  </div>
-)  
+  console.log(characters);
+
+  return (
+    <div className="App">
+      {characters.map((character) => (
+        <div key={character.id}>
+          <h2>{character.name}</h2>
+          <p>{character.species}</p>
+          <img src={character.image} alt={character.name} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
-
-
-
+export default App;
