3a083b443a2e4e4761ed58e848ad1f8e739dee31
Rubcs <rubcastellon@gmail.com>
2023-01-25 16:24:12 +0100

    add content to wallarock

--- REACT/wallarock/src/components/ProBanner.jsx
+++ REACT/wallarock/src/components/ProBanner.jsx
@@ -1,13 +1,26 @@
-import { BannerImage, BannerTop, BannerWrapper } from '../ui/Banner'
+import {
+  BannerImage,
+  BannerPro,
+  BannerProSubTitle,
+  BannerProTitle,
+  BannerTop,
+  BannerWrapper,
+  MoreInfoButton
+} from '../ui/Banner'
 
 const ProBanner = () => {
   return (
     <BannerWrapper>
       <BannerTop>
-        <BannerImage src="https://es.wallapop.com/images/home/walla_drill.svg" />
-        <BannerImage src="https://es.wallapop.com/images/home/walla_chair.svg" />
         <BannerImage src="https://es.wallapop.com/images/home/walla_toy.svg" />
+        <BannerImage src="https://es.wallapop.com/images/home/walla_chair.svg" />
+        <BannerImage src="https://es.wallapop.com/images/home/walla_drill.svg" />
       </BannerTop>
+      <BannerPro>
+        <BannerProTitle>WallaRock PRO</BannerProTitle>
+        <BannerProSubTitle>¿Tienes un negocio y quieres empezar a vender online?</BannerProSubTitle>
+        <MoreInfoButton>Más info</MoreInfoButton>
+      </BannerPro>
     </BannerWrapper>
   )
 }
--- REACT/wallarock/src/ui/Banner.jsx
+++ REACT/wallarock/src/ui/Banner.jsx
@@ -1,24 +1,62 @@
 import styled from 'styled-components'
 
 export const BannerWrapper = styled.div`
-background-color: rgb(30, 144, 255);
+background-color: #385EF9;
+height: 334px;
+margin-bottom: 10px;
+margin-top: 24px;
 display; flex;
 flex-direction: column;
 height: 100%;
 justify-content: space-around;
-border-radius: 24px;
+border-radius: 16px;
+visibility: inherit;
+width: 520;
 `
 
 export const BannerTop = styled.div`
   justify-content: space-evenly;
   align-items: center;
   display: flex;
+  height: 100%;
 `
 
 export const BannerImage = styled.img`
   width: 88px;
   height: 81px;
-  background-color: rgb(30, 144, 255);
   color: #253238;
-  background-color: #eceff1;
+  padding-top: 24px;
+  overflow: auto;
+`
+export const BannerPro = styled.div`
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  padding: 12px;
+  margin: 24px 12px 12px 12px;
+  border-radius: 12px;
+`
+export const BannerProTitle = styled.div`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 36px;
+  color: #385ef9;
+`
+export const BannerProSubTitle = styled.div`
+  font-size: 16px;
+  line-height: 22px;
+  color: #253238;
+  margin-top: 12px;
+`
+export const MoreInfoButton = styled.button`
+  height: 40px;
+  background: #385ef9;
+  border-radius: 21px;
+  color: #fff;
+  font-weight: 700;
+  font-size: 16px;
+  border: 0;
+  cursor: pointer;
+  margin-top: 8px;
 `
