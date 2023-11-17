48821dadff6f21a090403cc940d9a2fc10ab30ba
magicgithgub32 <rubcastellon@gmail.com>
2023-11-17 14:00:36 +0100

    create up to kyu-8-182

---
+++ CODEWARS/kyu-7/65-Sum-of-cubes.js
@@ -0,0 +1,20 @@
+// Write a function that takes a positive integer n, sums all the cubed values from 1 to n (inclusive), and returns that sum.
+
+// Assume that the input n will always be a positive integer.
+
+// Examples: (Input --> output)
+
+// 2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
+// 3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27
+
+const sumCubes = (n) => {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    let toSum = Math.pow(i, 3);
+
+    sum += toSum;
+  }
+  return sum;
+};
+
+console.log(sumCubes(3));
